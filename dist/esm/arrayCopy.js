62cf33c7d56599e29f9a8dd6d47c8ca0fc8b683e
electricessence <5899455+electricessence@users.noreply.github.com>
2025-08-16 09:01:47 -0700

    feat: modernize ESLint config and remove deprecated dependencies
    BREAKING CHANGE: Updated to ESLint 9 configuration and removed unused dependencies

--- dist/esm/arrayCopy.js
+++ dist/esm/arrayCopy.js
@@ -1,12 +1,9 @@
-"use strict";
 /*!
  * @author electricessence / https://github.com/electricessence/
  * Licensing: MIT
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const array_init_1 = tslib_1.__importDefault(require("@tsdotnet/array-init"));
-const exceptions_1 = require("@tsdotnet/exceptions");
+import arrayInit from '@tsdotnet/array-init';
+import { ArgumentNullException, ArgumentOutOfRangeException } from '@tsdotnet/exceptions';
 const CBN = 'Cannot be null.', CBL0 = 'Cannot be less than zero.';
 /**
  * Copies one array to another.
@@ -19,24 +16,24 @@ const CBN = 'Cannot be null.', CBL0 = 'Cannot be less than zero.';
  */
 function arrayCopyTo(source, destination, sourceIndex = 0, destinationIndex = 0, count = Infinity) {
     if (!source)
-        throw new exceptions_1.ArgumentNullException('source', CBN);
+        throw new ArgumentNullException('source', CBN);
     if (!destination)
-        throw new exceptions_1.ArgumentNullException('destination', CBN);
+        throw new ArgumentNullException('destination', CBN);
     if (sourceIndex < 0)
-        throw new exceptions_1.ArgumentOutOfRangeException('sourceIndex', sourceIndex, CBL0);
+        throw new ArgumentOutOfRangeException('sourceIndex', sourceIndex, CBL0);
     if (destinationIndex < 0)
-        throw new exceptions_1.ArgumentOutOfRangeException('destinationIndex', destinationIndex, CBL0);
+        throw new ArgumentOutOfRangeException('destinationIndex', destinationIndex, CBL0);
     const sourceLength = source.length;
     if (!sourceLength || count < 1)
         return destination;
     if (sourceIndex >= sourceLength)
-        throw new exceptions_1.ArgumentOutOfRangeException('sourceIndex', sourceIndex, 'Must be less than the length of the source array.');
+        throw new ArgumentOutOfRangeException('sourceIndex', sourceIndex, 'Must be less than the length of the source array.');
     // deal with ArrayLike issues.
     if (destination.length < 0)
-        throw new exceptions_1.ArgumentOutOfRangeException('destination.length', destination.length, CBL0);
+        throw new ArgumentOutOfRangeException('destination.length', destination.length, CBL0);
     const max = source.length - sourceIndex;
     if (isFinite(count) && count > max)
-        throw new exceptions_1.ArgumentOutOfRangeException('sourceIndex', sourceIndex, 'Source index + length cannot exceed the length of the source array.');
+        throw new ArgumentOutOfRangeException('sourceIndex', sourceIndex, 'Source index + length cannot exceed the length of the source array.');
     count = Math.min(count, max);
     const newLength = destinationIndex + count;
     if (newLength > destination.length)
@@ -57,7 +54,7 @@ function arrayCopyTo(source, destination, sourceIndex = 0, destinationIndex = 0,
 function arrayCopy(source, sourceIndex = 0, count = Infinity) {
     if (!source)
         return source; // may have passed zero? undefined? or null?
-    return arrayCopyTo(source, (0, array_init_1.default)(Math.min(count, Math.max(source.length - sourceIndex, 0))), sourceIndex, 0, count);
+    return arrayCopyTo(source, arrayInit(Math.min(count, Math.max(source.length - sourceIndex, 0))), sourceIndex, 0, count);
 }
 /**
  * Copies one array to another.
@@ -71,5 +68,5 @@ function arrayCopy(source, sourceIndex = 0, count = Infinity) {
 arrayCopy.to = function (source, destination, sourceIndex = 0, destinationIndex = 0, length = Infinity) {
     return arrayCopyTo(source, destination, sourceIndex, destinationIndex, length);
 };
-exports.default = arrayCopy;
+export default arrayCopy;
 //# sourceMappingURL=arrayCopy.js.map
